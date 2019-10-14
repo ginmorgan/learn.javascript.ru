9163f9f44d93cfcd507cd2c75da14be3100bd2dd
Pavel <49414734+ginmorgan@users.noreply.github.com>
2019-10-15 00:59:53 +0400

    js basis 1

---
+++ 1. JS basis/js/script.js
@@ -0,0 +1,37 @@
+//Что выведет этот скрипт?
+//let name = "Ilya";
+//
+//alert( `hello ${1}` ); // 1
+//
+//alert( `hello ${"name"}` ); // name
+//
+//alert( `hello ${name}` ); // Ilya
+
+//Чему будут равны переменные a, b, c и d в примере ниже?
+//let a = 1, b = 1;
+//
+//let c = ++a; // 2
+//let d = b++; // 1
+
+
+//Чему будут равны переменные a и x в примере ниже?
+//let a = 2;
+//let x = 1 + (a *= 2); //a=4 x=5
+
+//alert(5 > 4);//true
+//alert("ананас" > "яблоко");//false
+//alert("2" > "12");//true
+//alert(undefined == null);//true
+//alert(undefined === null);//false
+//alert(null == "\n0\n");//false
+//alert(null === +"\n0\n");//false
+
+//alert("Пример сообщения модального окна alert");
+//let age = prompt("Сколько тебе лет?",'');
+//alert(`Тебе ${age} лет!`);
+
+//let isBoss = confirm("Ты здесь главный?");
+//alert(isBoss);
+
+//let name = prompt('Как к вам обращаться?','');
+//alert(`Приветствуем тебя, о великий ${name}`);
